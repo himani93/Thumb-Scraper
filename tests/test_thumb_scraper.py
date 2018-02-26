1479db97fc2a1f25d6accfb48d74974ddba1414e
Himani Agrawal <himani93@gmail.com>
2018-02-26 21:39:04 +0530

    outside tests

---
+++ tests/test_thumb_scraper.py
@@ -0,0 +1,31 @@
+import pytest
+
+from thumb_scraper.exceptions import *
+from thumb_scraper.webpage import WebPage
+from thumb_scraper.scraper import ThumbScraper
+
+
+class TestThumbScraper(object):
+
+    def test_thumb_scraper_creation(self):
+        url = "https://yolaw-tokeep-hiring-env.herkoapp.com/"
+        scraper = ThumbScraper(url=url, pages={})
+        assert isinstance(scraper, ThumbScraper)
+        assert scraper._base_url == url
+        assert scraper._pages_to_scrape == {}
+        assert scraper._starting_page == "0"
+
+        with pytest.raises(InvalidURLException):
+            ThumbScraper(url="", pages="")
+
+        with pytest.raises(InvalidPageException):
+            ThumbScraper(url=url, pages="")
+
+    def test_get_web_page(self):
+        url = "https://yolaw-tokeep-hiring-env.herokuapp.com/"
+        scraper = ThumbScraper(url=url, pages={})
+        webpage = scraper._get_webpage()
+        assert isinstance(webpage, WebPage)
+
+    def test_scrape_page(self):
+        pass
